115d765af55a3a217cabb06d2aa91e89ed1709c0
Sergio Rodríguez <serrodale@gmail.com>
2023-04-08 16:18:16 +0200

    feat(ui): adds styles to `ToDoListItem`

--- ui/src/pages/home/ToDoListItem.tsx
+++ ui/src/pages/home/ToDoListItem.tsx
@@ -4,11 +4,16 @@ import { ToDo } from './shared/model/toDo';
 import useToDos from './shared/hooks/useToDos';
 
 const useClasses = createUseStyles({
-  grid: {
-    margin: 30,
-    display: 'grid',
-    gridTemplateColumns: 'repeat(3, 1fr)',
-    gap: 30,
+  container: {
+    display: 'flex',
+    alignContent: 'center',
+  },
+  checkbox: {
+    '-webkit-transform': 'scale(1.5)',
+    marginRight: 20,
+  },
+  titleWhenCompleted: {
+    textDecoration: 'line-through',
   },
 });
 
@@ -17,15 +22,20 @@ export default function Section({ toDo }: { toDo: ToDo }) {
   const { markToDoAsCompleted, markToDoAsUncompleted } = useToDos();
 
   return (
-    <>
+    <div className={classes.container}>
       <input
         type="checkbox"
         checked={toDo.completed}
+        className={classes.checkbox}
         onClick={() =>
           toDo.completed ? markToDoAsUncompleted(toDo.id) : markToDoAsCompleted(toDo.id)
         }
       />
-      {toDo.title}
-    </>
+      <span
+        className={toDo.completed ? classes.titleWhenCompleted : ''}
+      >
+        {toDo.title}
+      </span>
+    </div>
   );
 }
