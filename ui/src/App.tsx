921a75509ce409b6a32772e20791511c412ed52c
Sergio Rodríguez <serrodale@gmail.com>
2023-04-08 11:35:39 +0200

    feat(ui): defines global CSS styles

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Pages from './pages/Pages';
+import './globals.css';
 
 export default function App() {
   return (
