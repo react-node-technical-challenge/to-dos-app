cc370a057964b6f85b5e99c5f602af82e65d6839
Sergio Rodríguez <serrodale@gmail.com>
2023-04-08 16:08:20 +0200

    feat(ui): adds styles to `ToDoList`

--- ui/src/pages/home/ToDoList.tsx
+++ ui/src/pages/home/ToDoList.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { createUseStyles } from 'react-jss';
+import { colors } from '../../shared/constants/colors';
 import useToDos from './shared/hooks/useToDos';
 import ToDoListItem from './ToDoListItem';
 
 const useClasses = createUseStyles({
   list: {
-    
+    padding: 0,
   },
   listItem: {
-    
+    listStyleType: 'none',
+    paddingTop: 15,
+    paddingBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomStyle: 'solid',
+    borderBottomColor: colors.grey1,
   },
 });
 
@@ -20,7 +26,7 @@ export default function ToDoList() {
     <ul className={classes.list}>
       {toDos?.map((toDo) => {
         return (
-          <li className={classes.listItem}>
+          <li className={classes.listItem} key={toDo.id}>
             <ToDoListItem toDo={toDo} />
           </li>
         );
