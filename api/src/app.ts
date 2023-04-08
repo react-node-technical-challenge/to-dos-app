d831eb45553effd8218b46855e30ca63e884f2f6
Sergio Rodríguez <serrodale@gmail.com>
2023-04-08 16:25:19 +0200

    feat(api): validates environment variables in `app.ts`

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,10 +1,13 @@
 require('dotenv').config();
 
+import Joi from 'joi'M
 import cors from 'cors';
 import express from 'express';
 
 const app = express();
 const port = process.env.APP_PORT;
+
+validateEnvVariables();
 
 app.disable('x-powered-by');
 app.use(cors());
@@ -13,5 +16,26 @@ app.use(express.json());
 export const server = app.listen(port, () => {
   console.log(`API initialized on port ${port}!`);
 });
+
+function validateEnvVariables(): void {
+  const schema = Joi.object({
+    ENV: Joi.string().required(),
+    APP_PORT: Joi.number().required(),
+    DATABASE_HOST: Joi.string().not().empty().required(),
+    DATABASE_PORT: Joi.number().not().empty().required(),
+    DATABASE_PASSWORD: Joi.string().not().empty().required(),
+    DATABASE_NAME: Joi.string().not().empty().required(),
+    DATABASE_USER: Joi.string().not().empty().required(),
+  } satisfies Record<keyof typeof process.env, Joi.Schema>);
+
+  const { error } = schema.validate(process.env, {
+    allowUnknown: true,
+  });
+
+  if (error) {
+    console.error(error.message);
+    process.exit(1);
+  }
+}
 
 export default app;
